2a95874773b0ea806b4bb8a31d970021a341ce9a
RohanDoshi21 <rohan.doshi02@gmail.com>
2021-12-28 14:09:30 +0530

    Added unique user and password hashing

--- Task_App/src/models/user.js
+++ Task_App/src/models/user.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 mongoose.connect('mongodb://127.0.0.1:/task-app-api')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -34,5 +36,32 @@ const User = mongoose.model('User', {
         }
     },
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+
+    if (!user){
+        throw new Error('Unable to login')
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password)
+
+    if (!isMatched){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before saving/updating
+userSchema.pre('save', async function (next){
+    const user = this
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- Task_App/src/routers/user.js
+++ Task_App/src/routers/user.js
@@ -4,7 +4,7 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-    try{
+    try {
         await user.save()
         res.send(user)
     }
@@ -17,6 +17,16 @@ router.post('/users', async (req, res) => {
     //     res.status(400).send(e)
     // })
     // res.send('testing!')
+})
+
+router.post('/users/login/', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    }
+    catch (e) {
+        res.status(400).send()
+    }
 })
 
 router.get('/users', async (req, res) => {
@@ -54,7 +64,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!user) {
             return res.status(404).send()
@@ -66,11 +82,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res)=> {
-    try{
+router.delete('/users/:id', async (req, res) => {
+    try {
         const user = await User.findByIdAndDelete(req.params.id)
 
-        if (!user){
+        if (!user) {
             return res.status(401).send()
         }
 
