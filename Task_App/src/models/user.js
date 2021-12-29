2416fa3c80593248336b26bbbca77c8b1ba446dd
RohanDoshi21 <rohan.doshi02@gmail.com>
2021-12-29 13:12:17 +0530

    updated user patch and delete

--- Task_App/src/models/user.js
+++ Task_App/src/models/user.js
@@ -54,6 +54,26 @@ userSchema.methods.generateAuthToken = async function (){
     return token
 }
 
+// userSchema.methods.getPublicProfile = function () {
+//     const user = this
+//     const userObject = user.toObject()
+//
+//     delete userObject.password
+//     delete userObject.tokens
+//
+//     return userObject
+// }
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({email})
 
