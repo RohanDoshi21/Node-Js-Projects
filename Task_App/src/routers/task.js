27191a2f4f167f86c3c4b16acb1da175f915d9b8
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-01-07 21:17:01 +0530

    Added sorting

--- Task_App/src/routers/task.js
+++ Task_App/src/routers/task.js
@@ -18,11 +18,20 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /task?limit=10&skip=0
+// GET /task?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+        
     }
 
     try {
@@ -37,7 +46,12 @@ router.get('/tasks', auth, async (req, res) => {
 
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
             // match: {
             //     completed: false
             // }
