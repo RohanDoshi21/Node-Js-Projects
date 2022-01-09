d5deff9b16ec6ef5920aedfd3fe9f40ec5225d76
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-01-09 23:37:22 +0530

    Added Email Features

--- Task_App/src/models/user.js
+++ Task_App/src/models/user.js
@@ -58,7 +58,7 @@ userSchema.virtual('tasks', {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user.id.toString()}, 'rohandoshi21')
+    const token = jwt.sign({_id: user.id.toString()}, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({token})
     await user.save()
--- Task_App/src/routers/user.js
+++ Task_App/src/routers/user.js
@@ -4,11 +4,13 @@ const auth = require('../middleware/auth')
 const multer = require('multer')
 const router = new express.Router()
 const sharp = require('sharp')
+const { sendWelcomeEmail, sendCancellationEmail} = require('../emails/account')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({user, token})
     } catch (e) {
@@ -139,7 +141,7 @@ router.delete('/users/me', auth, async (req, res) => {
         // if (!user) {
         //     return res.status(401).send()
         // }
-
+        sendCancellationEmail(req.user.email, req.user.name)
         await req.user.remove()
         res.send(req.user)
 
@@ -173,7 +175,7 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
 
 router.get('/users/:id/avatar', async (req, res) => {
     try {
-        console.log("Test")
+        // console.log("Test")
         const user = await User.findById(req.params.id)
 
         if (!user || !user.avatar) {
