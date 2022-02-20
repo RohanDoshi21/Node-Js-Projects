98e26cf0910651c8a846f1b5b1136b3246d77347
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-02-20 21:56:50 +0530

    Socket Io basics

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection');
+
+    socket.emit('countUpdated', count)
+
+    socket.emit('message', "Welcome")
+
+    socket.on('increment', () => {
+        count++;
+        // socket.emit('countUpdated', count)  //this line will emit only to one connection
+        io.emit('countUpdated', count) // this will emit to all the connected clients
+    })
+})
+
+server.listen(port, () => {
     console.log('Server is up on PORT ' + port);
 })
