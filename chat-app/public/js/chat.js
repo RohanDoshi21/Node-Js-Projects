ee7b92dcdd2438b2459fc932fb0a9240380686b4
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-02-27 22:06:09 +0530

    Chat App Ui

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -13,10 +13,10 @@ socket.on('countUpdated', (count) => {
     console.log('The count has been updated:', count);
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log("Clicked");
-    socket.emit('increment');
-})
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log("Clicked");
+//     socket.emit('increment');
+// })
 
 socket.on('message', (message) => {
     console.log(message);
