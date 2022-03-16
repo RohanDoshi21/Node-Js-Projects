8ef8207a354dc0f3e9781cdb313055aca733a306
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-03-16 23:27:14 +0530

    Edit

--- monsters_api/db/index.js
+++ monsters_api/db/index.js
@@ -7,7 +7,7 @@ const {
   port,
 } = require("../secrets/db_configuration");
 
-const pool = new Pool(user, host, database, password, port);
+const pool = new Pool({ user, host, database, password, port });
 
 pool.query("SELECT * FROM monsters", (err, res) => {
   if (err) return console.log(err);
